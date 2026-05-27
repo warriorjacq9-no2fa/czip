2bf29efef9d18e3291c9c8ce85d6c736d259dfcf
warriorjacq9-no2fa <jflusche3180@student.pps.net>
2026-05-27 22:21:47 +0000

    Improve program design

--- zip.c
+++ zip.c
@@ -5,6 +5,11 @@
 #include <string.h>
 #include <zlib.h>
 
+static zip_cdr_t* cd;
+static size_t cd_len;
+static FILE* archive;
+static size_t files;
+
 uint16_t dos_time() {
     time_t now = time(NULL);
     struct tm *t = localtime(&now);
@@ -35,22 +40,26 @@ uint32_t do_crc32(const void *buf, size_t len) {
     return crc32(0L, (const Bytef*)buf, len);
 }
 
-int add_file(FILE* archive, FILE* f, char* filename, zip_cdr_t* cd, size_t cd_idx) {
+void quit(int code) {
+    if(cd) free(cd);
+    if(archive) fclose(archive);
+    exit(code);
+}
+
+void add_file(FILE* f, char* filename, size_t cd_idx) {
     uint16_t time = dos_time();
     uint16_t date = dos_date();
 
     if(fseek(f, 0, SEEK_END) < 0) {
         perror("Error seeking");
-        fclose(f);
-        return EXIT_FAILURE;
+        quit(EXIT_FAILURE);
     }
     size_t size = ftell(f);
     fseek(f, 0, SEEK_SET);
     void* data = malloc(size);
     if(fread(data, 1, size, f) != size) {
         perror("Error reading");
-        fclose(f);
-        return EXIT_FAILURE;
+        quit(EXIT_FAILURE);
     }
     fclose(f);
 
@@ -71,14 +80,14 @@ int add_file(FILE* archive, FILE* f, char* filename, zip_cdr_t* cd, size_t cd_id
                      -MAX_WBITS,
                      8,
                      Z_DEFAULT_STRATEGY) != Z_OK) {
-        return EXIT_FAILURE;
+        quit(EXIT_FAILURE);
     }
 
     int ret = deflate(&strm, Z_FINISH);
 
     if (ret != Z_STREAM_END) {
         deflateEnd(&strm);
-        return EXIT_FAILURE;
+        quit(EXIT_FAILURE);
     }
 
     size_t compressed_size = strm.total_out;
@@ -123,69 +132,33 @@ int add_file(FILE* archive, FILE* f, char* filename, zip_cdr_t* cd, size_t cd_id
 
     if(fwrite(&lfh, 1, sizeof(zip_lfh_t), archive) != sizeof(zip_lfh_t)) {
         perror("Error writing LFH");
-        fclose(archive);
-        return EXIT_FAILURE;
+        quit(EXIT_FAILURE);
     }
 
     if(fwrite(filename, 1, strlen(filename), archive) != strlen(filename)) {
         perror("Error writing filename");
-        fclose(archive);
-        return EXIT_FAILURE;
+        quit(EXIT_FAILURE);
     }
 
     if(fwrite(out, 1, size, archive) != size) {
         perror("Error writing file data");
-        fclose(archive);
-        return EXIT_FAILURE;
+        quit(EXIT_FAILURE);
     }
-    return EXIT_SUCCESS;
 }
 
-int main(int argc, char* argv[]) {
-    if(argc < 2) {
-        fprintf(stderr, "Usage: %s <outfile> [files]\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-    FILE* fil = fopen(argv[1], "wb+");
-    if(fil == NULL) {
-        fprintf(stderr, "Failed to create file %s\n", argv[1]);
-        return EXIT_FAILURE;
-    }
+void archive_write(char** filenames) {
+    /* Write central directory */
 
-    size_t files = argc - 2;
-    size_t cd_len = files * sizeof(zip_cdr_t);
-
-    if(files > 0) {
-
-        /* Prepare headers */
-        zip_cdr_t* cd = malloc(cd_len);
-        for(size_t i = 0; i < files; i++) {
-            FILE* f = fopen(argv[2 + i], "rb");
-            if(f == NULL) {
-                char msg[64];
-                snprintf(msg, 64, "Failed to open file %s", argv[2 + i]);
-                perror(msg);
-                continue;
-            }
-            add_file(fil, f, argv[2 + i], cd, i);
+    for(int i = 0; i < files; i++) {
+        if(fwrite(&cd[i], 1, sizeof(zip_cdr_t), archive) != sizeof(zip_cdr_t)) {
+            perror("Error writing central directory");
+            quit(EXIT_FAILURE);
         }
-
-        /* Write central directory */
-
-        for(int i = 0; i < files; i++) {
-            if(fwrite(&cd[i], 1, sizeof(zip_cdr_t), fil) != sizeof(zip_cdr_t)) {
-                perror("Error writing central directory");
-                fclose(fil);
-                return EXIT_FAILURE;
-            }
-            if(fwrite(argv[2 + i], 1, cd[i].name_len, fil) != cd[i].name_len) {
-                perror("Error writing filename");
-                fclose(fil);
-                return EXIT_FAILURE;
-            }
-            cd_len += cd[i].name_len;
+        if(fwrite(filenames[i], 1, cd[i].name_len, archive) != cd[i].name_len) {
+            perror("Error writing filename");
+            quit(EXIT_FAILURE);
         }
-        free(cd);
+        cd_len += cd[i].name_len;
     }
 
     /* Assemble and write EOCD */
@@ -197,16 +170,46 @@ int main(int argc, char* argv[]) {
         .cd_len_ldisk = files,
         .cd_len = files,
         .cd_size = cd_len,
-        .cd_offset = ftell(fil) - cd_len,
+        .cd_offset = ftell(archive) - cd_len,
         .comment_len = 0
     };
 
-    size_t s;
-    if((s = fwrite(&eocd, 1, sizeof(zip_eocd_t), fil)) != sizeof(zip_eocd_t)) {
-        printf("%llu", s);
+    if(fwrite(&eocd, 1, sizeof(zip_eocd_t), archive) != sizeof(zip_eocd_t)) {
         perror("Error writing EOCD");
-        fclose(fil);
+        quit(EXIT_FAILURE);
+    }
+}
+
+int main(int argc, char* argv[]) {
+    if(argc < 2) {
+        fprintf(stderr, "Usage: %s <outfile> [files]\n", argv[0]);
         return EXIT_FAILURE;
     }
-    fclose(fil);
+    archive = fopen(argv[1], "wb+");
+    if(archive == NULL) {
+        fprintf(stderr, "Failed to create file %s\n", argv[1]);
+        return EXIT_FAILURE;
+    }
+
+    files = argc - 2;
+    cd_len = files * sizeof(zip_cdr_t);
+    cd = malloc(cd_len);
+
+    if(files > 0) {
+        /* Prepare headers */
+        for(size_t i = 0; i < files; i++) {
+            FILE* f = fopen(argv[2 + i], "rb");
+            if(f == NULL) {
+                char msg[64];
+                snprintf(msg, 64, "Failed to open file %s", argv[2 + i]);
+                perror(msg);
+                continue;
+            }
+            add_file(f, argv[2 + i], i);
+        }
+    }
+
+    archive_write(&argv[2]);
+
+    quit(EXIT_SUCCESS);
 }
